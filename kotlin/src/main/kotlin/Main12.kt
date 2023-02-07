08ac4812d57a54f60c88cefb6ac64563886da62d
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-02-07 20:39:14 -0300

    Exercícios Concluídos

--- kotlin/src/main/kotlin/Main12.kt
+++ kotlin/src/main/kotlin/Main12.kt
@@ -1,37 +1,29 @@
-fun bonus(cargo:String){
-    if (cargo == "Gerente"){
-        println("Seu bônus é de R$ 2.000,00")
-        if (cargo == "Coordenador"){
-            println("Seu bônus é de R$ 1.500,00")
-        }
-        if (cargo == "Engenheiro"){
-            println("Seu bônus é de R$ 1.000,00")
-        }
-    }else if (cargo == "Estagiario"){
-        println("Seu bônus é de R$ 500,00")
+fun bonus(cargo: String): Float {
+    var bonus = 0f
+    if (cargo == "Gerente") {
+        bonus = 2000f
+    } else if (cargo == "Coordenador") {
+        bonus = 1500f
+    } else if (cargo == "Engenheiro") {
+        bonus = 1000f
+    } else if (cargo == "Estagiario") {
+        bonus = 500f
     }
+
+    return bonus
 }
 
-fun bonusTempo(cargo1:String, tempo:Int){
-    if (cargo1 == "Gerente" && tempo <= 2){
-        println("O bônus é de R$ 2.000,00")
-        if (cargo1 == "Gerente" && tempo >= 2){
-            println("O bônus é de R$ 3.000,00")
-        }
-        if (cargo1 == "Coordenador" && tempo < 1){
-        println("O bônus é de R$ 1.500,00")
-        }
-        if (cargo1 == "Coordenador" && tempo >= 1){
-            println("O bônus é de R$ 1.800,00")
-        }
-        if (cargo1 == "Engenheiro"){
-            println("Seu bônus é de R$ 1.000,00")
-        }
-    }else if (cargo1 == "Estagiario"){
-        println("Seu bônus é de R$ 500,00")
-    }
-}
+
 fun main() {
-    bonus("Estagiario")
-    bonusTempo("Gerente", 3)
+    val bonus = bonus("Gerente")
+    println(bonus)
+
 }
+
+
+
+
+
+
+
+
